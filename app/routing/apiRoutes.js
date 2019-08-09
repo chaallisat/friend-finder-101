6ff5b76251bbd079a2c4bb55b6e2cc58cafa4591
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-08-08 17:59:06 -0700

    added templates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,15 @@
+
+// import server.js file
 //GET route with url '/api/frineds' will isplay JSON of all possible friends
 //POST routes '/api/friends' will handle incosming survey results. WIll also handle compatibility logic
+
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+  });
+
+  app.post("/api/frineds", function(req, res) {
+      const newFriend = req.body;
+
+    friends.push(newFriend);
+    res.json(newFriend);
+  });
--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 //use npm packages express and path here
+const http = require("http");
+const express = require ("express");
+
+const app = express();
+
+const path = req.url;
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
